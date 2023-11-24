14a1150d7d06ad925fa994eb50f18e1d5abf6440
Lee <lees_taylor_18@yahoo.com>
2023-11-24 02:34:02 +0000

    task 7

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,17 +1,28 @@
 import random
 
-# Ask for a single letter input
-guess = input("Enter a single letter: ")
 
-# Check if input is valid
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
+def get_single_letter():
+    """Prompt the user for a single alphabetical character."""
+    guess = input("Enter a single letter: ")
+    if len(guess) == 1 and guess.isalpha():
+        return guess
+    else:
+        print("Oops! That is not a valid input.")
+        return None
+
+
+def choose_random_word(words):
+    """Choose and return a random word from the given list."""
+    return random.choice(words)
+
+
+# Define a list of fruits for the word choice
+fruits = ["banana", "orange", "apple", "pear", "grapes"]
+
+# Main execution
+user_guess = get_single_letter()
+if user_guess:
+    selected_word = choose_random_word(fruits)
+    print(f"Randomly selected word: {selected_word}")
 else:
-    print("Oops! That is not a valid input.")
-
-# Select a random word
-word_list = ["banana", "orange", "apple", "pear", "grapes"]
-word = random.choice(word_list)
-
-# Print the random word
-print(word)
+    print("No valid guess was made.")
