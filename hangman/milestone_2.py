2c085f43387a218aa515830cd99773cc906dafcc
Lee <lees_taylor_18@yahoo.com>
2023-11-24 02:02:21 +0000

    task 2

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -2,4 +2,8 @@ import random
 
 word_list = ["banana", "orange", "apple", "pear", "grapes"]
 
-print(word_list)
+# Step 3 and 4: Create the random.choice method and assign the result to a variable called word
+word = random.choice(word_list)
+
+# Step 5: Print out word
+print(word)
