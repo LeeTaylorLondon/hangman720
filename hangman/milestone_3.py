804ec78da4a1b284abebbe5fdd2e5f47d3b5c797
Lee <lees_taylor_18@yahoo.com>
2023-11-24 02:37:50 +0000

    milestone 3 task 2

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,13 +1,21 @@
 # milestone_3.py
 
+# Assuming the secret word is 'apple' for the example
+secret_word = "apple"
+
 # This loop will run indefinitely until a valid single alphabetical character is entered.
 while True:
     guess = input("Guess a letter: ")  # Prompt the user to guess a letter
 
     # Check if the guess is a single alphabetical character
     if len(guess) == 1 and guess.isalpha():
-        print(f"Good guess! You entered: {guess}")  # If valid, acknowledge the good guess
-        break  # Exit the loop
+        # If the letter guessed by the user is in the secret word
+        if guess in secret_word:
+            print(f"Good guess! {guess} is in the word.")  # Acknowledge the correct guess
+            break  # Exit the loop after a correct guess
+        else:
+            # Inform the user if the guess is not in the word
+            print(f"Sorry, {guess} is not in the word. Try again.")
     else:
-        # If the input is invalid, inform the user and continue the loop
+        # Inform the user of invalid input and continue the loop
         print("Invalid letter. Please, enter a single alphabetical character.")
