e86a75162c227f09c2c788d3e13283da6ce848c3
Lee <lees_taylor_18@yahoo.com>
2023-11-24 03:13:52 +0000

    milestone 5 task 2 + 3 + 4

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -1,13 +1,20 @@
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        """Initialize the Hangman class with a given word list and number of lives."""
+        """Initialize the Hangman class."""
         import random
         self._word = random.choice(word_list)
         self._word_guessed = ['_' for _ in self._word]
         self._num_letters = len(set(self._word))
         self.num_lives = num_lives
-        self.word_list = word_list
         self._list_of_guesses = []
+
+    def _is_guess_valid(self, guess):
+        """Check if the guess is valid."""
+        return len(guess) == 1 and guess.isalpha()
+
+    def _is_guess_new(self, guess):
+        """Check if the guess has not been tried before."""
+        return guess not in self._list_of_guesses
 
     def _update_word_guessed(self, guess):
         """Update the guessed word representation based on the user's guess."""
@@ -15,47 +22,37 @@ class Hangman:
             if letter == guess:
                 self._word_guessed[index] = guess
 
-    def _is_guess_correct(self, guess):
-        """Check if the guessed letter is in the word."""
-        return guess in self._word
+    def _handle_guess(self, guess):
+        """Handle the user's guess."""
+        guess = guess.lower()
+        self._list_of_guesses.append(guess)
 
-    def _handle_correct_guess(self, guess):
-        """Handle actions needed when the guess is correct."""
-        print(f"Good guess! {guess} is in the word.")
-        self._update_word_guessed(guess)
-        self._num_letters -= 1
-
-    def _handle_incorrect_guess(self, guess):
-        """Handle actions needed when the guess is incorrect."""
-        self.num_lives -= 1
-        print(f"Sorry, {guess} is not in the word.")
-        print(f"You have {self.num_lives} lives left.")
-
-    def _check_guess(self, guess):
-        """Check the guessed letter and respond accordingly."""
-        if self._is_guess_correct(guess):
-            self._handle_correct_guess(guess)
+        if guess in self._word:
+            print(f"Good guess! {guess} is in the word.")
+            self._update_word_guessed(guess)
+            self._num_letters -= 1
         else:
-            self._handle_incorrect_guess(guess)
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
-        """Prompt the user for a letter and validate it."""
+        """Prompt the user for a letter and handle the input."""
         while True:
             guess = input("Guess a letter: ")
-            if len(guess) != 1 or not guess.isalpha():
+
+            if not self._is_guess_valid(guess):
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self._list_of_guesses:
+            elif not self._is_guess_new(guess):
                 print("You already tried that letter!")
             else:
-                self._check_guess(guess.lower())
-                self._list_of_guesses.append(guess)
+                self._handle_guess(guess)
                 break
 
 
 def play_game(word_list):
     """Function to play the Hangman game."""
-    num_lives = 5
-    game = Hangman(word_list, num_lives)
+    game = Hangman(word_list)
 
     while True:
         if game.num_lives == 0:
