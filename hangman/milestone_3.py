829041124d50289c3ab3dd72dd6476206e36bebb
Lee <lees_taylor_18@yahoo.com>
2023-11-24 02:53:45 +0000

    milestone 3 task 3

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,21 +1,27 @@
-# milestone_3.py
+def check_guess(guess, word):
+    """Check if the guessed letter is in the word."""
+    guess = guess.lower()  # Convert the guess into lower case
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+        return True
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        return False
+
+
+def ask_for_input(word):
+    """Ask the user to input a letter and validate it."""
+    while True:  # Continue to ask for a letter until the user inputs a valid one
+        guess = input("Guess a letter: ")  # Prompt the user for a guess
+        if len(guess) == 1 and guess.isalpha():  # Check if guess is a single alphabetical character
+            if check_guess(guess, word):  # If the guess is in the word
+                break  # Exit the loop
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
 
 # Assuming the secret word is 'apple' for the example
 secret_word = "apple"
 
-# This loop will run indefinitely until a valid single alphabetical character is entered.
-while True:
-    guess = input("Guess a letter: ")  # Prompt the user to guess a letter
-
-    # Check if the guess is a single alphabetical character
-    if len(guess) == 1 and guess.isalpha():
-        # If the letter guessed by the user is in the secret word
-        if guess in secret_word:
-            print(f"Good guess! {guess} is in the word.")  # Acknowledge the correct guess
-            break  # Exit the loop after a correct guess
-        else:
-            # Inform the user if the guess is not in the word
-            print(f"Sorry, {guess} is not in the word. Try again.")
-    else:
-        # Inform the user of invalid input and continue the loop
-        print("Invalid letter. Please, enter a single alphabetical character.")
+# Start the user input process
+ask_for_input(secret_word)
